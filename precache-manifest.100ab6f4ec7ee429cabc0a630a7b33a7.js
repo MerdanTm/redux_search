1f068d02e0a2c296099fdfb4902c34482c3a61d3
Merdan Kurban <namainterior@gmail.com>
2021-05-18 01:48:58 +0400

    Updates

---
+++ precache-manifest.100ab6f4ec7ee429cabc0a630a7b33a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9ef67e74a840e562db91",
+    "url": "/redux_search/static/css/main.67bac037.chunk.css"
+  },
+  {
+    "revision": "9ef67e74a840e562db91",
+    "url": "/redux_search/static/js/main.ccb9bcd4.chunk.js"
+  },
+  {
+    "revision": "c4a535821b57eb26f83c",
+    "url": "/redux_search/static/js/runtime~main.28abed1e.js"
+  },
+  {
+    "revision": "b4d624c83c05a35755dc",
+    "url": "/redux_search/static/js/2.9e7f9fd3.chunk.js"
+  },
+  {
+    "revision": "acc34d39a6d9c5dec6bc5d4b014e8aab",
+    "url": "/redux_search/index.html"
+  }
+];
